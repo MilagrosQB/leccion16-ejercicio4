0fed984775faf8c8ef697b387ac9d2be6e369637
MilagrosQB <qbmilagros@gmail.com>
2016-08-25 15:10:10 -0500

    Ejercicio 4

---
+++ js/app.js
@@ -0,0 +1,4 @@
+	var tiempo = parseInt(prompt("Ingresa la duración de la llamada"));
+	var costoMinuto = parseInt(prompt("Ingresa el costo por minuto"));
+	var resultado = tiempo * costoMinuto;
+  	alert("El resultado es " + resultado);
